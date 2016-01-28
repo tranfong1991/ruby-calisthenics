60102ab0455fc9c78d7da7edef4477f41b23fffe
Andy Tran <tran_fong1991@yahoo.com>
2016-01-28 04:12:46 +0000

    working rock paper scissors

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,9 +1,11 @@
 module FunWithStrings
   def palindrome?
+    #downcase then get rid of non-words
     new_str = self.downcase.gsub(/\W/, "")
     return new_str == new_str.reverse
   end
   def count_words
+    #downcase then get rid of non-words except for space
     new_str = self.downcase.gsub(/[^\w\s]/, "")
     words = new_str.split(" ")
     frequencies = Hash.new(0)
@@ -19,6 +21,7 @@ module FunWithStrings
     new_str = self.downcase
     words = new_str.split(" ")
     
+    #sort characters in each word
     for i in (0...words.length) do
       words[i] = words[i].chars.sort.join
     end
@@ -27,10 +30,12 @@ module FunWithStrings
       h[k] = Array.new
     end
     
+    #push anagram into the same hash key
     for i in (0...words.length) do
       hash[words[i]].push(original[i])
     end
     
+    #contruct an array of arrays of anagrams
     arr = Array.new
     hash.each_value do |anagram|
       arr.push(anagram)
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,46 @@
 class RockPaperScissors
-
+  @@moves = ["R","P","S"]
+  
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    #check if any of the players have invalid move
+    if !@@moves.include?(player1[1]) || !@@moves.include?(player2[1])
+      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
+    
+    #when they have the same moves
+    if player1[1] == player2[1]
+      return player1
+    end
+    
+    case player1[1] 
+      when "R"
+        if player2[1] == "P"
+          return player2
+        else return player1
+        end
+      when "P"
+        if player2[1] == "R"
+          return player1
+        else return player2
+        end
+      else
+        if player2[1] == "R"
+          return player2
+        else return player1
+        end
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    #base case: when there is only one player
+    if tournament[0].is_a?(String) && tournament[1].is_a?(String)
+      return tournament
+    end
+    
+    return winner(tournament_winner(tournament[0]), tournament_winner(tournament[1]))
   end
 
 end
