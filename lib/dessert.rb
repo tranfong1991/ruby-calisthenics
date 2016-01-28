d526aa824a5803a09242a424d81241d3ba8c226e
Andy Tran <tran_fong1991@yahoo.com>
2016-01-28 03:23:30 +0000

    working dessert

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,44 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
+  end
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def calories
+    @calories
+  end
+  def calories=(new_calories)
+    @calories = new_calories
   end
   def healthy?
-    # your code here
+    @calories < 200
   end
   def delicious?
-    # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @calories = 5
+    @name = flavor + " jelly bean"
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(new_flavor)
+    @flavor = new_flavor
+  end
+  def delicious?
+    if @flavor == "licorice"
+      return false
+    end
+    return true
   end
 end
