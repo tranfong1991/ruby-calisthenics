fa8f5a318e8f559f3593aa0623469146e04155b7
Andy Tran <tran_fong1991@yahoo.com>
2016-01-28 02:30:52 +0000

    working fun_with_strings.rb

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,42 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    new_str = self.downcase.gsub(/\W/, "")
+    return new_str == new_str.reverse
   end
   def count_words
-    # your code here
+    new_str = self.downcase.gsub(/[^\w\s]/, "")
+    words = new_str.split(" ")
+    frequencies = Hash.new(0)
+    
+    words.each do |word|
+      frequencies[word] += 1
+    end
+    
+    return frequencies
   end
   def anagram_groups
-    # your code here
+    original = self.split(" ")
+    new_str = self.downcase
+    words = new_str.split(" ")
+    
+    for i in (0...words.length) do
+      words[i] = words[i].chars.sort.join
+    end
+    
+    hash = Hash.new do |h,k| 
+      h[k] = Array.new
+    end
+    
+    for i in (0...words.length) do
+      hash[words[i]].push(original[i])
+    end
+    
+    arr = Array.new
+    hash.each_value do |anagram|
+      arr.push(anagram)
+    end
+    
+    return arr
   end
 end
 
